e7ca20d17310f891fac177be45d071e7c82f411b
Muhammad Junaid Aslam <15-10648@formanite.fccollege.edu.pk>
2024-10-03 02:34:25 +0500

    refactor: Update relation name in AssetsService findOne method

--- src/assets/services/assets.service.ts
+++ src/assets/services/assets.service.ts
@@ -93,7 +93,7 @@ export class AssetsService {
   async findOne(id: string): Promise<Asset> {
     const asset = await this.assetsRepository.findOne({
       where: { id },
-      relations: ['photos', 'client', 'customer', 'type']
+      relations: ['photos', 'client', 'customer', 'assetType']
     });
     if (!asset) {
       throw new NotFoundException(`Asset #${id} not found`);
